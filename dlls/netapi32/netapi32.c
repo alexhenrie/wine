89f435d1292aba0f84c645d182d27f795b8644f4
Alex Henrie <alexhenrie24@gmail.com>
2019-09-11 19:28:30 -0600

    NetUserGetInfo

--- dlls/netapi32/netapi32.c
+++ dlls/netapi32/netapi32.c
@@ -1331,10 +1331,7 @@ NetUserGetInfo(LPCWSTR servername, LPCWSTR username, DWORD level,
         /* get data */
         status = NetUserGetInfo(servername, username, 0, (LPBYTE *) &ui0);
         if (status != NERR_Success)
-        {
-            NetApiBufferFree(ui0);
             return status;
-        }
         name_sz = lstrlenW(ui0->usri0_name) + 1;
 
         /* set up buffer */
@@ -1374,10 +1371,7 @@ NetUserGetInfo(LPCWSTR servername, LPCWSTR username, DWORD level,
        /* get data */
         status = NetUserGetInfo(servername, username, 0, (LPBYTE *) &ui0);
         if (status != NERR_Success)
-        {
-            NetApiBufferFree(ui0);
             return status;
-        }
         name_sz = lstrlenW(ui0->usri0_name) + 1;
         home_dir_sz = GetEnvironmentVariableW(L"HOME", NULL,0);
         /* set up buffer */
